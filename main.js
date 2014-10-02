6c0d36ceb61d006d0d02aded8b900c1f35aa9951
Matthew Kaufer <matthew@kaufer.org>
2014-10-01 22:46:41 -0400

    Prettified js - Todo, turn this into an NPM module

--- main.js
+++ main.js
@@ -4,34 +4,34 @@ var savePath = "images/";
 
 var header = {
 	"User-Agent": "IcoGrab"
-};//universal header
+}; //universal header
 
-function mkdir(path){
-  try {
-    fs.mkdirSync(path);
-  } catch(e) {
-    //don't do anything - folder exists.
-  }
+function mkdir(path) {
+	try {
+		fs.mkdirSync(path);
+	} catch (e) {
+		//don't do anything - folder exists.
+	}
 }
 
-function makeOptions(u){
+function makeOptions(u) {
 	return {
-		url:u,
-		headers:header
+		url: u,
+		headers: header
 	};
 }
 
-function path(username){//hassle-free way to generate the paths
-	return savePath + username + ".jpg";//.png was breaking for some images
+function path(username) { //hassle-free way to generate the paths
+	return savePath + username + ".jpg"; //.png was breaking for some images
 }
 
-function grabIcon(username, force){//grabs the user's icon from GitHub and saves it in an images folder, if it's not already there - force is a bool whether or not to force overwrite
-	force = force || false;//default to no force, meaning won't overwrite images if it exists already - will save time and resources, etc.
+function grabIcon(username, force) { //grabs the user's icon from GitHub and saves it in an images folder, if it's not already there - force is a bool whether or not to force overwrite
+	force = force || false; //default to no force, meaning won't overwrite images if it exists already - will save time and resources, etc.
 
-	if(!force && fs.existsSync(path(username))){//if the user doesn't want to force override and the file exists
+	if (!force && fs.existsSync(path(username))) { //if the user doesn't want to force override and the file exists
 		console.log("Image " + username + ".png exists!")
-		return;//stop doing stuff - we don't want to overwrite
-    }
+		return; //stop doing stuff - we don't want to overwrite
+	}
 
 	var options = makeOptions("https://api.github.com/users/" + username);
 
@@ -39,11 +39,11 @@ function grabIcon(username, force){//grabs the user's icon from GitHub and saves
 		if (!error && response.statusCode == 200) {
 			body = JSON.parse(body);
 			var avatar = body.avatar_url;
-			avatar = avatar.substring(0, avatar.indexOf("?"));//gets rid of the stuff at the end, just in case
+			avatar = avatar.substring(0, avatar.indexOf("?")); //gets rid of the stuff at the end, just in case
 
 			var avOptions = makeOptions(avatar);
 
-			request(avOptions, function(){
+			request(avOptions, function() {
 				console.log("Image " + path(username) + " saved");
 			}).pipe(fs.createWriteStream(path(username)));
 
@@ -57,5 +57,5 @@ function grabIcon(username, force){//grabs the user's icon from GitHub and saves
 
 mkdir(savePath);
 
-grabIcon("mjkaufer",true);
+grabIcon("mjkaufer", true);
 grabIcon("pwstegman", true);
