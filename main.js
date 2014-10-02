d5c09bf8b1a503ac29b9476ff41fcacf8810132f
Matthew Kaufer <matthew@kaufer.org>
2014-10-01 22:31:31 -0400

    Image saving, dir checking, gitignore

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-var unirest = require('unirest');
-var http = require('http');	
-var savePath = "/";
+var request = require('request');
+var fs = require('fs');
+var savePath = "images/";
+
+var header = {
+	'User-Agent': 'IcoGrab'
+};//universal header
+
+var mkdir = function (path) {
+  try {
+    fs.mkdirSync(path);
+  } catch(e) {
+    //don't do anything - folder exists.
+  }
+}
+
+function makeOptions(u){
+	return {
+		url:u,
+		headers:header
+	};
+}
 
 function grabIcon(username, force){//grabs the user's icon from GitHub and saves it in an images folder, if it's not already there - force is a bool whether or not to force overwrite
-//TODO - implement a hash check to see if we need to update the image or no
-//for now, we'll be content with simply getting the image
 
-	unirest.get('https://api.github.com/users/' + username)
-	.headers({'User-Agent':'Icon Grab'})
-	.end(function(response){
-		var avatar = response.body.avatar_url;
-		avatar = avatar.substring(0, avatar.indexOf("?"));
-		console.log(avatar);
 
-	});
+
+	var options = makeOptions('https://api.github.com/users/' + username);
+
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			body = JSON.parse(body);
+			var avatar = body.avatar_url;
+			avatar = avatar.substring(0, avatar.indexOf("?"));
+
+			var avOptions = makeOptions(avatar);
+			request(avOptions, function(){
+				console.log("Image saved");
+			}).pipe(fs.createWriteStream(savePath + username + '.png'))
+
+		}
+	}
+
+	request(options, callback);
 
 }
+
+
+mkdir(savePath);
 
 grabIcon("mjkaufer",true);
